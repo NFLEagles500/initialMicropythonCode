a6a82db2ce6e3bfd304c222a0c0c73778f9e962a
NFLEagles500 <NFLEagles500@gmail.com>
2023-07-03 19:32:25 -0600

    Update main.py
    This will set the correct "pin" for the built-in LED, and if a Pico W, it'll join wifi, get utc time, and obtain offset depending on your timezone offset

--- main.py
+++ main.py
@@ -1,21 +1,66 @@
+#If you are using a Pico W, be sure to create the envSecrets.py with ssid, wifipsw, timeApiUrl.
+#timeApiUrl is 'https://timeapi.io/api/TimeZone/zone?timeZone={INSERT TIME ZONE HERE}'
+
 from machine import Pin, UART, RTC
-import utime
-import time
+from utime import sleep, sleep_ms, time, localtime
 import network
-import secrets
-import os
+import envSecrets
 import ntptime
-import sys
+import usys
 import uos
 
-#Setting onboard LED depending on Pico type
-devCheck = os.uname()
+#Setting defaults depending on which pico
+devCheck = uos.uname()
 if 'Pico W' in devCheck.machine:
+    dev = 'picow'
     led = Pin('LED', Pin.OUT)
 else:
+    dev = 'pico'
     led = Pin(25, Pin.OUT)
 led.value(0)
 
+def utcToLocal(type):
+    #get the offset from timeapi.io, using your timezone
+    response = urequests.get(envSecrets.timeApiUrl)
+    localUtcOffset = response.json()['currentUtcOffset']['seconds']
+    localTime = localtime(time() - localUtcOffset)
+    if type == 'time':
+        return f'{localTime[3]:02d}:{localTime[4]:02d}:{localTime[5]:02d}'
+    elif type == 'date':
+        return f'{localTime[0]}/{localTime[1]}/{localTime[2]:02d}'
+    else:
+        return f'{localTime[0]}/{localTime[1]}/{localTime[2]:02d} {localTime[3]:02d}:{localTime[4]:02d}:{localTime[5]:02d}'
+
+def connect():
+    #Connect to WLAN
+    while wlan.isconnected() == False:
+        #Be sure to set {hostname} below
+        wlan.config(hostname=f'{hostname}')
+        wlan.active(True)
+        wlan.connect(envSecrets.ssid, envSecrets.wifipsw)
+        iter = 1
+        while wlan.isconnected() == False:
+            print(f'Not Connected...{iter}')
+            iter += 1
+            if iter == 10:
+                print('Reached max waiting for wifi')
+                break
+            sleep(1)
+        ip = wlan.ifconfig()[0]
+        print(f'{network.hostname()} is connected on {ip}')
+
+if dev == 'picow':
+    wlan = network.WLAN(network.STA_IF)
+    wlan.disconnect()
+    connect()
+    try:
+        ntptime.settime()
+        print(f"{utcToLocal('datetime')}")
+    except Exception as error:
+        print(error)
+        pass
+
+#Start coding.  Blink added for example
 while True:
   led.toggle()
   sleep(1)
